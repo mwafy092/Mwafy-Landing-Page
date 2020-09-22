2584b7ef55bbcc86a4be8831e75dea997b7af7b0
mwafy <mwafy@pop-os.localdomain>
2020-09-22 17:58:27 +0200

    Updates

---
+++ precache-manifest.b5843c193df7def6153f72f37cc6bc75.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b822901e6171856862fc93e32a432ee4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b01b52621ab19e176b69",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "e467e40ac15e3763d797",
+    "url": "/static/css/main.8f7d79e9.chunk.css"
+  },
+  {
+    "revision": "b01b52621ab19e176b69",
+    "url": "/static/js/2.539f113d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.539f113d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e467e40ac15e3763d797",
+    "url": "/static/js/main.fad504b0.chunk.js"
+  },
+  {
+    "revision": "66f570d605abe0b8f7be",
+    "url": "/static/js/runtime-main.dee07914.js"
+  },
+  {
+    "revision": "8666526f9d24a8f84a294a3b3f8b5ae9",
+    "url": "/static/media/aircnc1.8666526f.png"
+  },
+  {
+    "revision": "8085c3b32972c4cd42015ad59d336985",
+    "url": "/static/media/aircnc2.8085c3b3.png"
+  },
+  {
+    "revision": "16dd9a44e032e47a3ff6055e00a88389",
+    "url": "/static/media/aircnc3.16dd9a44.png"
+  },
+  {
+    "revision": "ad3904b12feba1d6a8675f82f02241bc",
+    "url": "/static/media/github.ad3904b1.svg"
+  },
+  {
+    "revision": "47765b4e2b5ed96369bbf5b29e19b2ab",
+    "url": "/static/media/hotel1.47765b4e.png"
+  },
+  {
+    "revision": "b4a3e003d70dc03d176c82d2aa5a6b41",
+    "url": "/static/media/hotel2.b4a3e003.png"
+  },
+  {
+    "revision": "bacd0476452dfc6d88f41d7ab5d723f4",
+    "url": "/static/media/hotel3.bacd0476.png"
+  },
+  {
+    "revision": "5965bdc99f5d9d916acad2b650b12dc0",
+    "url": "/static/media/linkedin.5965bdc9.svg"
+  },
+  {
+    "revision": "04de47b12c055a7e5b20352a0d5ce11e",
+    "url": "/static/media/mail.04de47b1.svg"
+  },
+  {
+    "revision": "69a4cae3963f1e97042ffecfc793313b",
+    "url": "/static/media/profile.69a4cae3.jpg"
+  },
+  {
+    "revision": "10c267cb4ef2544367c42e60208eaa38",
+    "url": "/static/media/travel1.10c267cb.png"
+  },
+  {
+    "revision": "247b5e436b833eaf788eb59e0df4ac39",
+    "url": "/static/media/travel2.247b5e43.png"
+  },
+  {
+    "revision": "f9e5a60f6c4b8419e9c0cd51ac629136",
+    "url": "/static/media/travel3.f9e5a60f.png"
+  }
+]);
