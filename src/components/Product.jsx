9f845247fd6d6dfd3cd206c19453c57f7bd52e77
mwafy092 <mwafy092@gmail.com>
2020-09-26 23:34:02 +0200

    add animation to projects

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../styles/Product.scss';
 function Product({ name, image, link, git }) {
     return (
-        <div className='product'>
+        <div className='product animate1'>
             <h2>{name}</h2>
             <img src={image} alt='' />
             <a href={git} target='_blank'>
