e6ca0b71f2ef1908e2e2ee4d936f0f61d8d91766
mwafy092 <mwafy092@gmail.com>
2020-09-26 11:56:21 +0200

    add nav effect

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,29 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/Nav.scss';
 import Mail from '../styles/images/mail.svg';
-class Nav extends Component {
-    state = {};
-    render() {
-        return (
-            <div className='nav'>
-                <div className='logo'>
-                    mwafy
-                    <span>Lab</span>
-                </div>
-                <nav>
-                    <ul>
-                        <li>home</li>
-                        <li>about</li>
-                        <li>works</li>
-                        <li>contact</li>
-                        <li className='mail'>
-                            <img src={Mail} alt='' />
-                        </li>
-                    </ul>
-                </nav>
+
+const Nav = () => {
+    const [nav, setNav] = useState(false);
+
+    const changeNav = () => {
+        if (window.scrollY >= 60) {
+            setNav(true);
+        } else {
+            setNav(false);
+        }
+    };
+
+    window.addEventListener('scroll', changeNav);
+    return (
+        <div className={nav ? 'scrollEffect nav' : 'nav'}>
+            <div className='logo'>
+                mwafy
+                <span>Lab</span>
             </div>
-        );
-    }
-}
+            <nav>
+                <ul>
+                    <li>home</li>
+                    <li>about</li>
+                    <li>works</li>
+                    <li>contact</li>
+                    <li className='mail'>
+                        <a href='mailto:mwafy092@gmail.com' target='_blank'>
+                            <img src={Mail} alt='' />
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
 
 export default Nav;
